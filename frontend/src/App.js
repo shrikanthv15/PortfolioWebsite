0641c241c2a06f20f89baa130bb12c787c6ec71b
shrikanthv15 <shri.vilvadrinath@gmail.com>
2024-03-27 02:42:16 +0530

    pushing p

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import HomePage from './HomePage';
 import './App.css'; // Import your main styling file
 import ChatRoom from './ChatRoom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   return (
